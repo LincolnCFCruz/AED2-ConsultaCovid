067eeabdfe189c8c6765496dab9dd3bd49c863ba
Pedro Igor <55034603+ORZAYAZ@users.noreply.github.com>
2021-05-14 10:59:36 -0300

    Create main.c

---
+++ Fonte/main.c
@@ -0,0 +1,20 @@
+#include arvoreCovid.h
+
+int escolha()
+{
+	int resposta = 0;
+	exibirMenu();
+	scanf("%d", &resposta);
+	return resposta;
+}
+
+int menu()
+{
+	
+}
+
+int main (void)
+{
+	int opcao = 0;
+	return 0;
+}
